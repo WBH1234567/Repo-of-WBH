90c8e58c16c59b3357c88876bbb9368856931820
WBH <wbh15881624750@163.com>
2020-01-14 21:41:56 +0800

    add something

---
+++ test.c
@@ -0,0 +1,18 @@
+/*===============================================================
+*   Copyright (C) 2020 All rights reserved.
+*   
+*   文件名称：test.c
+*   创 建 者：Wangbohai
+*   创建日期：2020年01月14日
+*   描    述：
+*
+*   更新日志：
+*
+================================================================*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+
+first add;
+second add;
