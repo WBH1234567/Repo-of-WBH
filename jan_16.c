9e4c60ec9e1f0784a30a69019e0642674394895f
WBH <wbh15881624750@163.com>
2020-01-16 20:50:51 +0800

    jan_16.c

---
+++ jan_16.c
@@ -0,0 +1,17 @@
+/*===============================================================
+*   Copyright (C) 2020 All rights reserved.
+*   
+*   文件名称：jan_16.c
+*   创 建 者：Wangbohai
+*   创建日期：2020年01月16日
+*   描    述：
+*
+*   更新日志：
+*
+================================================================*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+
+Today is JAN 16 2020,writed at branch test.
